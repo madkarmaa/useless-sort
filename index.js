72d30072e549d196f4277d605db18766e9f5a230
☆ mk ☆ <100418457+madkarmaa@users.noreply.github.com>
2023-10-14 23:41:54 +0200

    feat: don't modify original array

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const uselessSort = (arr) => {
+    arr = [...arr];
     for (let i = 0; i < Math.floor(Math.random() * 100); i++) arr.push(arr[Math.floor(Math.random() * arr.length)]);
     return arr.sort(() => Math.random() - 0.5);
 };
