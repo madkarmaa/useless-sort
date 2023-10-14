474431096976dd6a26463b873e8366e46e619b84
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2023-10-14 23:17:32 +0200

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const uselessSort = (arr) => {
+    for (let i = 0; i < Math.floor(Math.random() * 100); i++) arr.push(arr[Math.floor(Math.random() * arr.length)]);
+    return arr.sort(() => Math.random() - 0.5);
+};
+
+module.exports = uselessSort;
