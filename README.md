aa29b40d6ec463589fa4dffefa9a02b995a24bd8
madkarmaa <madkarma99@gmail.com>
2023-10-15 18:39:13 +0200

    feat: extend Array.prototype

--- README.md
+++ README.md
@@ -11,9 +11,11 @@ npm i git+https://github.com/madkarmaa/useless-sort.git
 ## Usage
 
 ```js
-const uselessSort = require('useless-sort');
+const _ = require('useless-sort');
 
-console.log(uselessSort([1, 2, 3])); // Example output: [2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2]
+// Extends Array.prototype with the uselessSort() method
+console.log([1, 2, 3].uselessSort());
+// Example output: [2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2]
 ```
 
 ## Contributing
--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const uselessSort = (arr) => {
-    if (!arr) throw new Error('No array was passed');
-    if (!arr.length) throw new Error('The array is empty');
-    if (!Array.isArray(arr)) throw new TypeError("That's not an array");
-
-    arr = [...arr];
+Array.prototype.uselessSort = function () {
+    const arr = [...this];
     for (let i = 0; i < Math.floor(Math.random() * 100); i++) arr.push(arr[Math.floor(Math.random() * arr.length)]);
     return arr.sort(() => Math.random() - 0.5);
 };
 
-module.exports = uselessSort;
+module.exports = {};
